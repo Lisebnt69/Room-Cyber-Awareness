07b02747fb01aea8fde0d7910acac0b9dc8f4ba6
Lise <liseb73@gmail.com>
2026-04-10 18:10:35 +0200

    fix: update SuperAdmin

--- src/pages/ScenarioBuilder.jsx
+++ src/pages/ScenarioBuilder.jsx
@@ -117,21 +117,7 @@ function BlockPreview({
     }
   }
 
-  const loadReadyScenario = (preset) => {
-    setScenario({
-      ...preset,
-      framework: preset.framework || '',
-      sector: preset.sector || '',
-      fakeEmailSender: preset.fakeEmailSender || '',
-      fakeEmailSubject: preset.fakeEmailSubject || '',
-      fakeEmailBody: preset.fakeEmailBody || '',
-      fakeLinkLabel: preset.fakeLinkLabel || '',
-      fakeLinkUrl: preset.fakeLinkUrl || '',
-      fakeLinkHover: preset.fakeLinkHover || '',
-      quizQuestions: preset.quizQuestions || [],
-      photoHotspots: preset.photoHotspots || [],
-    })
-  }
+
 
   const loadReadyScenario = (preset) => {
     setScenario({
